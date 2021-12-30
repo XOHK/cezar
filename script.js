b296bf699a139dde46708f02e3f06dd3c02a6a85
Ibrahim Doskhoev <ibrahim.doskhoev@gmail.com>
2021-12-30 22:39:04 +0300

    начали оживлять HTML страничку с помощью JS

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+  const encryptButton = document.getElementById("encryptButton");
+  encryptButton.addEventListener("click", encrypt);
+};
+
+function encrypt() {
+  const textElement = document.getElementById("text");
+  // TODO: добавить сюда код, который шифрует сообщение
+}
