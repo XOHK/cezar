4759ccde0a351786ac4d954ef6f61b172d5220a2
Ibrahim Doskhoev <ibrahim.doskhoev@gmail.com>
2022-01-01 00:28:31 +0300

    Реализовали код на JS

--- script.js
+++ script.js
@@ -1,9 +1,135 @@
+const ENG_CON = [
+  "b",
+  "c",
+  "d",
+  "f",
+  "g",
+  "h",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "v",
+  "w",
+  "x",
+  "z",
+];
+const ENG_VOI = ["a", "e", "i", "o", "u", "y"];
+
+const RUS_CON = [
+  "б",
+  "в",
+  "г",
+  "д",
+  "ж",
+  "з",
+  "й",
+  "к",
+  "л",
+  "м",
+  "н",
+  "п",
+  "р",
+  "с",
+  "т",
+  "ф",
+  "х",
+  "ц",
+  "ч",
+  "ш",
+  "щ",
+];
+const RUS_VOI = ["а", "е", "ё", "и", "о", "у", "ы", "э", "ю", "я"];
+
 window.onload = function () {
   const encryptButton = document.getElementById("encryptButton");
   encryptButton.addEventListener("click", encrypt);
+  
+  const decryptButton = document.getElementById("decryptButton");
+  decryptButton.addEventListener("click", decrypt);
+  
+  const clearButton = document.getElementById("clearButton");
+  clearButton.addEventListener("click", clear);
+  
+  const copyButton = document.getElementById("copyButton");
+  copyButton.addEventListener("click", copy);
 };
 
 function encrypt() {
   const textElement = document.getElementById("text");
-  // TODO: добавить сюда код, который шифрует сообщение
+  const text = textElement.value;
+
+  const decryptedText = document.getElementById("decryptedText");
+  decryptedText.value = cezar(text);
+}
+
+function decrypt() {
+  const textElement = document.getElementById("text");
+  const text = textElement.value;
+
+  const decryptedText = document.getElementById("decryptedText");
+  decryptedText.value = cezar(text, false);
+}
+
+function clear() {
+  const textElement = document.getElementById("text");
+  textElement.value = ''
+  const decryptedText = document.getElementById("decryptedText");
+  decryptedText.value = ''
+}
+
+function copy() {
+  const decryptedText = document.getElementById("decryptedText");
+  // decryptedText.select();
+  navigator.clipboard.writeText(decryptedText.value)
+}
+
+function cezar(text, isEncript = true) {
+  const newWords = [];
+  const words = text.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    let newWord = "";
+    const wordLength = word.length;
+    for (let j = 0; j < wordLength; j++) {
+      const symbol = word[j];
+      const newSymbol = getNewSymbol(
+        symbol,
+        wordLength * (isEncript ? 1 : -1),
+        [ENG_CON, ENG_VOI, RUS_CON, RUS_VOI]
+      );
+      newWord += newSymbol;
+    }
+    newWords.push(newWord);
+  }
+  return newWords.join(" ");
+
+  function getNewSymbol(symbol, offset, charsList) {
+    let newSymbol = symbol;
+    const lowerSymbol = symbol.toLowerCase();
+
+    for (let i = 0; i < charsList.length; i++) {
+      const chars = charsList[i];
+      if (chars.indexOf(lowerSymbol) !== -1) {
+        newSymbol = checkSymbol(symbol, offset, chars);
+        break;
+      }
+    }
+    return newSymbol;
+  }
+
+  function checkSymbol(symbol, offset, arr) {
+    const index = arr.indexOf(symbol.toLowerCase());
+    const newIndex = (index + offset + arr.length) % arr.length;
+    const newSymbol = arr[newIndex];
+    return symbol === symbol.toUpperCase()
+      ? newSymbol.toUpperCase()
+      : newSymbol;
+  }
 }
